3105ea68f36b67e906821faf7f8e03df14c73af9
Charlotte Murray <80853179+charlottekmurray@users.noreply.github.com>
2021-12-08 15:58:52 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+var elem = document.querySelector(".carousel");
+var flkty = new Flickity(elem, {
+  wrapAround: true,
+  autoPlay: 3000,
+  fade: true,
+  prevNextButtons: false,
+  pageDots: false,
+  setGallerySize: false
+});
